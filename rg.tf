43217905614356574918a9b97cb766ad7b64aa46
diliproutray12 <diliproutray8@gmail.com>
2026-06-05 10:38:14 +0530

    dilip rg leave

--- rg.tf
+++ rg.tf
@@ -6,7 +6,3 @@ resource "azurerm_resource_gropu" "main"{
   name = "drgbanda"
   location = "eastus"
   }
-resource "azurerm_resource_gropu" "main"{
-  name = "drgbander"
-  location = "eastus"
-  }
